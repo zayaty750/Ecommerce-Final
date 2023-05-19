361d8e5ca04e7edaadf44ff901c427938baf6683
zeyad <zeyad2103469@miuegypt.edu.eg>
2023-05-19 14:58:26 +0200

    partials

---
+++ Server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const session = require('express-session')
+// express app
+const app = express();
+app.use(session({ secret: 'Your_Secret_Key' }));
+// listen for requests
+app.listen(8080);
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
