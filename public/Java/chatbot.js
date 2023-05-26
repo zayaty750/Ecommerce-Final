a46d9b9a6df42690d52a1c9e61a90c15574def35
Mark <mark2107794@miuegypt.edu.eg>
2023-05-26 22:41:32 +0300

    chatbotjsArrayCreated

--- public/Java/chatbot.js
+++ public/Java/chatbot.js
@@ -1,3 +1,10 @@
 var sendBtn=document.getElementById('sendBtn');
 var textbox=document.getElementById('textbox');
 var chatContainer = document.getElementById('chatContainer');
+var user={message:""};
+
+var arrayOfPossibleMessage = [
+    {message:"hi",response:"hello"},
+    {message:"how are you?",response:"good"},
+    {message:"what is your name?",response:"I'am a chatbot!"}
+]
