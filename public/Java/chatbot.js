c07382bc590d735ce131dab8cd2d999822ac2f1d
Mark <mark2107794@miuegypt.edu.eg>
2023-05-26 22:42:31 +0300

    chatbotjs+sendMessageFunction

--- public/Java/chatbot.js
+++ public/Java/chatbot.js
@@ -8,3 +8,15 @@ var arrayOfPossibleMessage = [
     {message:"how are you?",response:"good"},
     {message:"what is your name?",response:"I'am a chatbot!"}
 ]
+
+//the message will displayed in a span and the span will displayed into a div and the div will be displayed in the chat container
+function sendMessage(userMessage){
+    var messageElement = document.createElement('div');
+    messageElement.style.textAlign="right";
+    messageElement.style.margin="10px";
+ 
+    messageElement.innerHTML="<span> You: </span>"+
+                             "<span>" +userMessage+ "</span>";
+ 
+     chatContainer.appendChild(messageElement);
+ }
