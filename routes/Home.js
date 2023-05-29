6b621e89190cb28948596a07a81722b8e29f2ec3
zeyad <zeyad2103469@miuegypt.edu.eg>
2023-05-29 16:06:33 +0200

    edit code structure #1

--- routes/Home.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"; 
-const router = Router();
-
-router.get('/',function(req,res,next)
-{
-    res.render('pages/Home',{ user: (req.session.user === undefined ? "" : req.session.user) })
-});
-
-export default router; 
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,10 @@ router.get('/', (req, res)=> {
   res.render('pages/index');
 });
 
+router.get('/Home',function(req,res,next)
+{
+    res.render('pages/Home',{ user: (req.session.user === undefined ? "" : req.session.user) })
+});
+
+
 export default router;
