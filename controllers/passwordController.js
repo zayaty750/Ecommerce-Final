694c96162205c5c25a4cefd072626b2274b82b20
Mark <mark2107794@miuegypt.edu.eg>
2023-06-05 14:57:55 +0300

    "sendForgotPasswordLink Added"

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -11,7 +11,62 @@ const getForgotPasswordView = asyncHandler((req,res)=>{
     res.render('forgot-password');
 })
 
+/**
+ * @desc  Send Forgot Password Link
+ * @route  /password/forgot-password
+ * @method  POST
+ * @access  public
+ */
+
+const sendForgotPasswordLink = asyncHandler(async(req,res)=>{
+    //console.log(req.body.email);
+    const user = await User.findOne({email:req.body.email});
+    if (!user) {
+        return res.status(404).json({ message: "user not found" });
+      }
+      const secret = process.env.JWT_SECRET_KEY + user.password;
+      const token = jwt.sign({ email: user.email, id: user.id }, secret, {
+        expiresIn: "10m",
+      });
+
+      const link = `http://localhost:5000/password/reset-password/${user._id}/${token}`;
+
+     res.json({message: "click on the link ",resetPasswordLink: link});
+    
+
+    //   const transporter = nodemailer.createTransport({
+    //     service: "gmail",
+    //     auth: {
+    //        user: process.env.USER_EMAIL,
+    //        pass: process.env.USER_PASS,
+    //     }
+    //  });
+
+    //  const mailOptions = {
+    //     from: process.env.USER_EMAIL,
+    //     to: user.email,
+    //     subject: "Reset Password",
+    //     html: `<div>
+    //               <h4>Click on the link below to reset your password</h4>
+    //               <p>${link}</p>
+    //           </div>`
+    //   }
+    
+    //   transporter.sendMail(mailOptions, function(error, success){
+    //     if(error){
+    //       console.log(error);
+    //       res.status(500).json({message: "something went wrong"});
+    //     } else {
+    //       console.log("Email sent: " + success.response);
+    //       res.render("link-send");
+    //     }
+    //   });
+    
+});
+
+
 
 export{
-    getForgotPasswordView
+    getForgotPasswordView,
+    sendForgotPasswordLink
 };
