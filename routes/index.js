438022fba08b67cb842202f98b853878912fd9d3
zeyad <zeyad2103469@miuegypt.edu.eg>
2023-06-07 13:19:24 +0200

    inject  data from db to the sliders

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import {Cart}
 from  "../models/cart-model.js";
+import Product from '../models/products_model.js';
 
 
 const router = Router();
@@ -16,7 +17,24 @@ router.get('/', (req, res)=> {
 router.get('/Home',function(req,res,next)
 {
     let cart = new Cart(req.session.cart ? req.session.cart : {});
-    res.render('pages/Home',{ user: (req.session.user === undefined ? "" : req.session.user) ,qt: cart.totalQty});
+    
+    const products = Product.find({})
+    .then((products) => {
+
+      if (products.length > 0) {
+        products.sort((a, b) => {
+          const dateA = new Date(a.createdAt);
+          const dateB = new Date(b.createdAt);
+          return dateA - dateB;
+        });
+      }
+
+      
+      //res.json(products);
+      res.render("pages/Home", { products: products, user: (req.session.user === undefined ? "" : req.session.user),qt: cart.totalQty}); 
+
+    }) //get all products
+   
 });
 
 // Aboutus page
